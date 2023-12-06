c1d835bcbb73ed669b86642766641919c7f0f99d
Ridha15 <ridha.ds22@duk.ac.in>
2023-12-06 23:43:32 +0530

    final commit

--- main.py
+++ main.py
@@ -1,76 +1,65 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 import os
 import tensorflow as tf
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from keras.utils import to_categorical
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import EarlyStopping
-
 from tqdm import tqdm
 from keras.utils import to_categorical
 
-image_dir='cropped'
-lionel_messi = os.listdir(image_dir+ '/lionel_messi')
-maria_sharapova = os.listdir(image_dir+ '/maria_sharapova')
-roger_federer = os.listdir(image_dir+ '/roger_federer')
-serena_williams = os.listdir(image_dir+ '/serena_williams')
-virat_kohli = os.listdir(image_dir+ '/virat_kohli')
-print("--------------------------------------\n")
+def load_and_preprocess_image(img_path):
+    image = cv2.imread(img_path)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize(img_siz)
+    return np.array(image)
 
-dataset=[]
-label=[]
-img_siz=(128,128)
+# Data augmentation using ImageDataGenerator
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
 
+image_dir = 'cropped'
+class_directories = {
+    'lionel_messi': 0,
+    'maria_sharapova': 1,
+    'roger_federer': 2,
+    'serena_williams': 3,
+    'virat_kohli': 4
+}
 
-for i , image_name in tqdm(enumerate(lionel_messi),desc="Lionel Messi"):
-    if(image_name.split('.')[1]=='png'):
-        image=cv2.imread(image_dir+'/lionel_messi/'+image_name)
-        image=Image.fromarray(image,'RGB')
-        image=image.resize(img_siz)
-        dataset.append(np.array(image))
-        label.append(0)
+dataset = []
+label = []
+img_siz = (128, 128)
+
+for class_name, class_label in class_directories.items():
+    images = os.listdir(os.path.join(image_dir, class_name))
+    for i, image_name in tqdm(enumerate(images), desc=class_name.capitalize()):
+        if image_name.split('.')[-1].lower() == 'png':
+            img_path = os.path.join(image_dir, class_name, image_name)
+            original_image = load_and_preprocess_image(img_path)
         
-        
-for i ,image_name in tqdm(enumerate(maria_sharapova),desc="Maria Sharapova"):
-    if(image_name.split('.')[1]=='png'):
-        image=cv2.imread(image_dir+'/maria_sharapova/'+image_name)
-        image=Image.fromarray(image,'RGB')
-        image=image.resize(img_siz)
-        dataset.append(np.array(image))
-        label.append(1)
+            for _ in range(10):  
+                augmented_image = datagen.random_transform(original_image)
+                dataset.append(augmented_image)
+                label.append(class_label)
 
-for i , image_name in tqdm(enumerate(roger_federer),desc="Roger Federer"):
-    if(image_name.split('.')[1]=='png'):
-        image=cv2.imread(image_dir+'/roger_federer/'+image_name)
-        image=Image.fromarray(image,'RGB')
-        image=image.resize(img_siz)
-        dataset.append(np.array(image))
-        label.append(2)
-for i , image_name in tqdm(enumerate(serena_williams),desc="Serena Williams"):
-    if(image_name.split('.')[1]=='png'):
-        image=cv2.imread(image_dir+'/serena_williams/'+image_name)
-        image=Image.fromarray(image,'RGB')
-        image=image.resize(img_siz)
-        dataset.append(np.array(image))
-        label.append(3)
-for i , image_name in tqdm(enumerate(virat_kohli),desc="Virat Kohli"):
-    if(image_name.split('.')[1]=='png'):
-        image=cv2.imread(image_dir+'/virat_kohli/'+image_name)
-        image=Image.fromarray(image,'RGB')
-        image=image.resize(img_siz)
-        dataset.append(np.array(image))
-        label.append(4)
-        
-dataset=np.array(dataset)
+dataset = np.array(dataset)
 label = np.array(label)
 
 print("--------------------------------------\n")
-print('Dataset Length: ',len(dataset))
-print('Label Length: ',len(label))
+print('Augmented Dataset Length: ', len(dataset))
+print('Augmented Label Length: ', len(label))
 print("--------------------------------------\n")
+
+
 
 print("--------------------------------------\n")
 print("Train-Test Split")
@@ -85,23 +74,13 @@ x_test=x_test.astype('float')/255
 y_train_encoded = to_categorical(y_train, num_classes=5)
 y_test_encoded = to_categorical(y_test, num_classes=5)
 class_names = ['Lionel Messi', 'Maria Sharapova', 'Roger Federer', 'Serena Williams', 'Virat Kohli']
-# Data augmentation
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
 
-# Early stopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-
-# Model definition
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256, activation='relu'),
@@ -110,14 +89,15 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(5, activation='softmax')
 ])
 
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5, restore_best_weights=True)
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Training with data augmentation and early stopping
 print("--------------------------------------\n")
 print("Training Started.\n")
 history = model.fit(
     datagen.flow(x_train, y_train_encoded, batch_size=32),
-    epochs=50,
+    epochs=30,
     validation_data=(x_test, y_test_encoded),
     callbacks=[early_stopping]
 )
@@ -130,13 +110,14 @@ print("Model Evaluation Phase.\n")
 loss, accuracy = model.evaluate(x_test, y_test_encoded)
 print(f'Accuracy: {round(accuracy*100, 2)}')
 
-# Classification report
 y_pred = model.predict(x_test)
 y_pred_classes = np.argmax(y_pred, axis=1)
 print('Classification Report\n', classification_report(y_test, y_pred_classes, target_names=class_names))
 print("--------------------------------------\n")
 
-# Model Prediction
+model.save("celebrity_model.h5")
+saved_model = tf.keras.models.load_model("celebrity_model.h5")
+
 print("--------------------------------------\n")
 print("Model Prediction.\n")
 def make_prediction(img, model):
@@ -150,5 +131,9 @@ def make_prediction(img, model):
     predicted_class_name = class_names[predicted_class]
     return predicted_class_name
 
-print(make_prediction('cropped/fed_test.png', model))
+print(make_prediction('cropped/fed_test.png', saved_model))
+print(make_prediction('cropped/kohli_test.jpeg', saved_model))
+print(make_prediction('cropped/maria_test.jpg', saved_model))
+print(make_prediction('cropped/messi_test.jpeg', saved_model))
+print(make_prediction('cropped/serena_test.jpg', saved_model))
 print("--------------------------------------\n")
